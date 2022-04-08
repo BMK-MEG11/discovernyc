bb1265d1d202a1fb4bb7c9abb03bf3485837dc82
moritzro <97282836+moritzro@users.noreply.github.com>
2022-04-08 19:51:17 +0200

    added slick js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Slick Slider
+document.ready(function(){
+    document.getElementByClassName(".teaser-slider").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slideToScroll: 1
+    });
+  });
