fcc9ad05e48fe23fb254d6213b6f24e0cceaa55a
moritzro <97282836+moritzro@users.noreply.github.com>
2022-04-09 15:48:02 +0200

    changes

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ $(document).ready(function(){
       infinte: true,
       slidesToShow: 1,
       slidesToScroll: 1,
+      arrows: true,
       autoplay: true,
       autoplaySpeed: 4000
     });
   });
+
+// Typewriter
+const textToReplace = document.getElementById('text-replace')
+const replacementText = ['BRKLN', 'QNS', 'MNHTTN']
+let i = 0
+let j = 0
+
+
+function loop () {
+    if(i < replacementText.length) {
+        console.log(replacementText[i])
+
+        if(j < replacementText[i].length) {
+            console.log(replacementText[i][j])
+            j++
+        }
+
+        if (j == replacementText[i]) {
+
+        }
+    }
+    setTimeout(loop, 500)
+}
+
+loop()
