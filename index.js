e345b36d24dc6f55e082ef9e16b56f4f63cf987a
moritzro <97282836+moritzro@users.noreply.github.com>
2022-04-11 14:09:53 +0200

    chore(index.js): added responsiveness slider

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 $(document).ready(function(){
     $('.teaser-slider').slick({
       infinte: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
+      slidesToShow: 3,
+      slidesToScroll: 3,
       arrows: true,
       autoplay: true,
-      autoplaySpeed: 4000
+      autoplaySpeed: 4000,
+      responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 710,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+      }
+      ]
     });
   });
 
