68f1b6fd9f91692a486e91a6793f3eb1be930757
moritzro <97282836+moritzro@users.noreply.github.com>
2022-05-10 14:43:53 +0200

    changes

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
 // Header add background
-const header = document.getElementById('header')
+const header = document.getElementById("header");
 
-window.onscroll = function headerBackground () {
-    let pageOffset = window.pageYOffset
+window.onscroll = function headerBackground() {
+  let pageOffset = window.pageYOffset;
 
-    if(pageOffset >= 50) {
-        header.classList.add('scrolled')
-    } 
-    
-    else {
-        header.classList.remove('scrolled')
-    }
-}
+  if (pageOffset >= 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+};
 
 // Typewriter
-const textToReplace = document.getElementById('text-replace')
-const replacementText = ['NYC', 'BRKLN', 'QNS', 'MNHTTN']
-let i = 0
-let j = 0
-let currentText = []
-let isDeleting = false
+const textToReplace = document.getElementById("text-replace");
+const replacementText = ["NYC", "BRKLN", "QNS", "MNHTTN"];
+let i = 0;
+let j = 0;
+let currentText = [];
+let isDeleting = false;
 
-function loop () {
-    textToReplace.innerHTML = currentText.join('')
+function loop() {
+  textToReplace.innerHTML = currentText.join("");
 
-    if (i < replacementText.length) {
-
-        if (!isDeleting && j <= replacementText[i].length) {
-            currentText.push(replacementText[i][j])
-            j++
-        }
-        
-        if (isDeleting && j <= replacementText[i].length) {
-            currentText.pop(replacementText[i][j])
-            j--
-        }
-
-        if (j == replacementText[i].length) {
-            isDeleting = true
-        }
-
-        if (isDeleting && j === 0) {
-            currentText = []
-            isDeleting = false
-            i++
-
-            if (i === replacementText.length) {
-                i = 0
-            }
-        }
+  if (i < replacementText.length) {
+    if (!isDeleting && j <= replacementText[i].length) {
+      currentText.push(replacementText[i][j]);
+      j++;
     }
-    const speed = Math.random() * (500 - 400) + 400
-    setTimeout(loop, speed)
+
+    if (isDeleting && j <= replacementText[i].length) {
+      currentText.pop(replacementText[i][j]);
+      j--;
+    }
+
+    if (j == replacementText[i].length) {
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentText = [];
+      isDeleting = false;
+      i++;
+
+      if (i === replacementText.length) {
+        i = 0;
+      }
+    }
+  }
+  const speed = Math.random() * (500 - 400) + 400;
+  setTimeout(loop, speed);
 }
 
-loop()
+loop();
--- slider.js
+++ slider.js
@@ -1,27 +1,27 @@
 // Slick Slider
-$(document).ready(function(){
-    $('.teaser-slider').slick({
-      infinte: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      arrows: true,
-      autoplay: true,
-      autoplaySpeed: 4000,
-      responsive: [
-        {
+$(document).ready(function () {
+  $(".teaser-slider").slick({
+    infinte: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 710,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-      }
-      ]
-    });
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
+});
